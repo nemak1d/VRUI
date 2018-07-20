879f7aae41accbf03d75df57ef041a750f5e38c4
nemak1d <nemak1d.sys@gmail.com>
2018-07-21 02:38:32 +0900

    Mathf拡張クラス追加

---
+++ VRUI/Assets/01_Scripts/Common/Editor/Test/MathfExtentionsTest.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using N1D;
+
+public class MathfExtentionsTest
+{
+
+    [Test]
+    public void RepeatPI2_範囲境界上_Min()
+	{
+		Assert.AreEqual(0.0f, MathfExtentions.RepeatPI2(0.0f));
+    }
+
+	[Test]
+	public void RepeatPI2_範囲境界上_Max()
+	{
+		Assert.AreEqual(0.0f, MathfExtentions.RepeatPI2(MathfExtentions.PI2));
+	}
+
+	[Test]
+	public void RepeatPI2_範囲未満()
+	{
+		Assert.AreEqual(MathfExtentions.PI2 - 0.1f, MathfExtentions.RepeatPI2(-0.1f));
+	}
+	
+	[Test]
+	public void RepeatPI2_1週回って範囲未満()
+	{
+		Assert.AreEqual(0.0f, MathfExtentions.RepeatPI2(-MathfExtentions.PI2 - MathfExtentions.PI2));
+	}
+
+	[Test]
+	public void RepeatPI2_半週回って範囲未満()
+	{
+		Assert.AreEqual(Mathf.PI, MathfExtentions.RepeatPI2(-Mathf.PI));
+	}
+}
---
+++ VRUI/Assets/01_Scripts/Common/MathfExtentions.cs
@@ -0,0 +1,48 @@
+﻿//##################################################
+/*
+ *	MathfExtentions.cs
+ *
+ *	develop by nemak1d_sys
+ *
+ * -------------------------------------------------
+ * Mathf拡張.
+ */
+//##################################################
+using UnityEngine;
+
+namespace N1D
+{
+	/// <summary>
+	/// Mathf拡張.
+	/// </summary>
+	public struct MathfExtentions 
+	{
+		public const float PI2 = Mathf.PI * 2.0f;
+
+		/// <summary>
+		/// 0-2πの範囲に置き換えたAtan2.
+		/// </summary>
+		/// <param name="y">y軸差分</param>
+		/// <param name="x">x軸差分</param>
+		/// <returns>角度(0-2π)[rad]</returns>
+		public static float Atan2To2PI(float y, float x)
+		{
+			float radian = Mathf.Atan2(y, x);
+			if (0.0f > radian)
+			{
+				radian += PI2;
+			}
+			return radian;
+		}
+
+		/// <summary>
+		/// 0-2πの範囲をループさせて値を収める.
+		/// </summary>
+		/// <param name="radian">任意の角度[rad]</param>
+		/// <returns>角度(0-2π)[rad]</returns>
+		public static float RepeatPI2(float radian)
+		{
+			return Mathf.Repeat(radian, PI2);
+		}
+	}
+}
