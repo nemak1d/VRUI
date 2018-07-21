ece3689487346a9292332fd2a8a0f7201d094aad
nemak1d <nemak1d.sys@gmail.com>
2018-07-21 23:11:13 +0900

    入力UI設定

---
+++ VRUI/Assets/01_Scripts/UI/TypeConfiguration.cs
@@ -0,0 +1,52 @@
+﻿//##################################################
+/*
+ *	TypeConfiguration.cs
+ *
+ *	develop by nemak1d_sys
+ *
+ * -------------------------------------------------
+ * タイプ設定.
+ */
+//##################################################
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace N1D
+{
+	[DisallowMultipleComponent]
+	public class TypeConfiguration : MonoBehaviour 
+	{
+		[Header("Binding Components")]
+		[SerializeField]
+		Text captionField = null;
+		[Space]
+
+		[Header("Local Parameters")]
+		[SerializeField]
+		string caption = string.Empty;
+		[SerializeField]
+		char outputCharacter = 'n';
+		
+		public string Caption
+		{
+			set
+			{
+				caption = value;
+				captionField.text = value;
+			}
+			get { return caption; }
+		}
+
+		public char Output
+		{
+			set { outputCharacter = value; }
+			get { return outputCharacter; }
+		}
+
+		void Start()
+		{
+			Caption = caption;
+		}
+	}
+}
+
